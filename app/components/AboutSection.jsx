a074285a951122ededcb6e55733eba3030ac6787
Ashish <dasashish54321@gmail.com>
2024-04-01 05:40:44 +0530

    change

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -27,13 +27,14 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2 text-[#ADB7BE]">
-        <li>Sikha ’O’ Anusandhan University (ITER)</li>
-        <li>Master's in Computer Application(MCA) (GPA: 10 / 8.27)</li><br/>
+        <li>Sikha &apos;O&apos; Anusandhan University (ITER)</li>
+        <li>Master&apos;s in Computer Application(MCA) (GPA: 10 / 8.27)</li>
         <li>NIIS Institute Of Information Sc. and, Bhubaneswar</li>
         <li>Bachelor of Science ( Computer Science) (GPA: 10 / 7.34)</li>
       </ul>
     ),
   },
+  
   {
     title: "Certifications",
     id: "certifications",
@@ -43,7 +44,7 @@ const TAB_DATA = [
         <li>Certified from Computer Application Center(CAC), Bhubaneswar in Python </li>
       </ul>
     ),
-  },
+  }
 ];
 
 const AboutSection = () => {
@@ -58,21 +59,21 @@ const AboutSection = () => {
 
   return (
     <section className="text-[#ADB7BE]" id="about">
-    <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-7 mt-15 ">
-      <Image src="/images/Ashish5.jpg" alt="Ashish image" className="rounded-[3rem]" width={500} height={500} />
-      <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-7 mt-15 ">
+        <Image src="/images/Ashish5.jpg" alt="Ashish image" className="rounded-[3rem]" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-2 mt-8">About Me</h2>
           <p className="text-sm lg:text-base lg:text-lg text-justify w-650 h-470">
-          I am a recent Computer Science graduate with a strong passion for Java Fullstack development. 
-          My fascination with technology has driven me to master the Java programming language. As a 
-          fresh talent in the industry, I offer a unique perspective, a hunger for learning, and a robust 
-          work ethic. I possess solid skills in front-end technologies like HTML, CSS, Bootstrap, JavaScript, 
-          and ReactJS, along with expertise in back-end development using Spring Boot and Java.  I am a quick learner, 
-          adaptable, and always looking for new challenges to tackle. I am always willing to go the extra mile to ensure 
-          for the success. I am excited about the opportunity to work on innovative projects and contribute to the ever-evolving 
-          world of technology. I am eager to contribute to innovative projects and be part of a dynamic team. Thank you for considering 
-          my profile. I look forward to the opportunity to collaborate and create impactful solutions. Let's create something amazing together! 
+            I am a recent Computer Science graduate with a strong passion for Java Fullstack development. 
+            My fascination with technology has driven me to master the Java programming language. As a 
+            fresh talent in the industry, I offer a unique perspective, a hunger for learning, and a robust 
+            work ethic. I possess solid skills in front-end technologies like HTML, CSS, Bootstrap, JavaScript, 
+            and ReactJS, along with expertise in back-end development using Spring Boot and Java.  I&apos;am a quick learner, 
+            adaptable, and always looking for new challenges to tackle. I am always willing to go the extra mile to ensure 
+            for the success. I&apos;am excited about the opportunity to work on innovative projects and contribute to the ever-evolving 
+            world of technology. Thank you for considering my profile. I look forward to the opportunity to collaborate and create impactful solutions. Let&apos;s create something amazing together! 
           </p>
+
           <div className="flex flex-row justify-start mt-3">
             <TabButton
               selectTab={() => handleTabChange("skills")}
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,58 +1,56 @@
 "use client";
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation';
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection =()=>{
     return(
         <section>
-            <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <div className='col-span-7 place-self-center text-center sm:text-left'>
-                    <div className='flex justify-start ml-[2.5rem] lg:ml-[.2rem] mb-[10px]'><h1>ASHISH</h1></div>
-                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-[42px] font-secondary
-                    font-semibold uppercase leading-[1] '>
-                        Hello,<span className='text-transparent bg-clip-text bg-gradient-to-r from-lime-400 to-green-800'> I am a{" "}</span>
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <div className="flex justify-start ml-[2.5rem] lg:ml-[.2rem] mb-[10px]"><h1>ASHISH</h1></div>
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-[42px] font-secondary
+                    font-semibold uppercase leading-[1]">
+                        Hello,
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-lime-400 to-green-800">
+                            {" "}
+                            I am a{" "}
+                        </span>
                         <br></br>
-                        <TypeAnimation className='text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-800 to-blue-500 '
+                        <TypeAnimation className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-800 to-blue-500"
                             sequence={[
-                                // Same substring at the start will only be typed out once, initially
-                                // 'Ashish',
-                                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                                'Web Developer',
+                                1000, 
+                                "Web Developer",
                                 1000,
-                                'Mobile Developer',
+                                "Mobile Developer",
                                 1000,
-                                'Java Full-stack Developer',
+                                "Java Full-stack Developer",
                                 1000
                             ]}
                             wrapper="span"
                             speed={70}
-                           // style={{ fontSize: '3rem' }}
                             repeat={Infinity}
                             />
                     </h1>
-                    <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl text-justify'>Crafting dynamic web solutions with Java 
-                        expertise and as a Java Full Stack Developer 
-                        demonstrates my passion for learning and expertise in Java and Spring Boot,
-                         with a focus on delivering innovative solutions. Let's 
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl text-justify">Crafting dynamic web solutions with Java 
+                        expertise and as a Java Full Stack Developer demonstrates my passion for learning and expertise in Java and Spring Boot,
+                         with a focus on delivering innovative solutions. Let&apos;s &apos;
                         collaborate to bring innovative web solutions to life!
                     </p>
                     <div>
-                        {/* <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-emerald-500 via-blue-500 to-green-600 hover:bg-slate-200 text-white'>Hire Me</button> */}
-                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-emerald-500 via-blue-500 to-green-600 hover:bg-slate-800 text-white mt-3'>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-emerald-500 via-blue-500 to-green-600 hover:bg-slate-800 text-white mt-3">
                         <a download href="Pdf/Ashish_CV.pdf" className="button button--flex">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download Resume</span>
                         </a>
-                        {/* <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2' >Download Resume</span> */}
                         </button>
                     </div>
                 </div>
-                <div className='col-span-5 place-self-center mt-10 lg:mt-0'>
+                <div className="col-span-5 place-self-center mt-10 lg:mt-0">
                     <div className="rounded-full bg-[#181818] w-[350px] h-[350px] lg:w-[370px] lg:h-[370px] relative">
                     <Image
                       src="/images/Ashish.png"
                       alt="Ashish image"
-                      className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                      className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                       width={180}
                       height={160}
                     />
